1512def93b25a608e5730b9402a504323a2f408d
Melekai1 <melekaiyewondwossen13@gmail.com>
2026-03-31 15:50:35 -0700

    Added JavaScript to program

---
+++ cs111.js
@@ -0,0 +1,99 @@
+function isCorrect() { //Is called when the user selects the correct answer
+    document.getElementById("Correct").innerHTML = "This answer is correct!";
+}
+
+function isIncorrect1A() { //Give feedback if the user selects A for question 1
+    document.getElementById("Incorrect1A").innerHTML = "This answer is incorrect. A variable is allowed to change its value during execution.";
+}
+
+function isIncorrect1C() { //Give feedback if the user selects C for question 1
+    document.getElementById("Incorrect1C").innerHTML = "This answer is incorrect. A variable holds a specific value, akin to a box containing some contents. A loop executes some code repeatedly."
+}
+
+function isIncorrect1D() { //Give feedback if the user selects D for question 1
+    document.getElementById("Incorrect1D").innerHTML = "This answer is incorrect. A function is a programming block that uses variables, but a function itself is not a variable.";
+}
+
+function isIncorrect2A() { //Give feedback if the user selects A for question 2
+    
+}
+
+function isIncorrect2B() {
+
+}
+
+function isIncorrect2C() {
+
+}
+
+function isIncorrect3A() {
+
+}
+
+function isIncorrect3B() {
+
+}
+
+function isIncorrect3D() {
+
+}
+
+function isIncorrect4A() {
+
+}
+
+function isIncorrect4C() {
+
+}
+
+function isIncorrect4D() {
+
+}
+
+function isIncorrect5A() {
+
+}
+
+function isIncorrect5C() {
+
+}
+
+function isIncorrect5D() {
+
+}
+
+function isIncorrect6A() {
+
+}
+
+function isIncorrect6B() {
+
+}
+
+function isIncorrect6C() {
+
+}
+
+function isIncorrect7A() {
+
+}
+
+function isIncorrect7B() {
+
+}
+
+function isIncorrect7D() {
+
+}
+
+function isIncorrect8A() {
+
+}
+
+function isIncorrect8B() {
+
+}
+
+function isIncorrect8D() {
+
+}
