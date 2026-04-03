15665a069a022f1e3d0a04e7a9c5f92446741716
Melekai1 <melekaiyewondwossen13@gmail.com>
2026-04-02 20:12:27 -0700

    Made additions to cs111.html and cs111.js files

--- cs111.js
+++ cs111.js
@@ -1,5 +1,33 @@
-function isCorrect() { //Is called when the user selects the correct answer
-    document.getElementById("Correct").innerHTML = "This answer is correct!";
+function isCorrect1B() { //Is called when the user selects the correct answer for question 1
+    document.getElementById("Correct1B").innerHTML = "This answer is correct!";
+}
+
+function isCorrect2D() { //Is called when the user selects the correct answer for question 2
+    document.getElementById("Correct2D").innerHTML = "This answer is correct!";
+}
+
+function isCorrect3C() { //Is called when the user selects the correct answer for question 3
+    document.getElementById("Correct3C").innerHTML = "This answer is correct!";
+}
+
+function isCorrect4B() { //Is called when the user selects the correct answer for question 4
+    document.getElementById("Correct4B").innerHTML = "This answer is correct!";
+}
+
+function isCorrect5B() { //Is called when the user selects the correct answer for question 5
+    document.getElementById("Correct5B").innerHTML = "This answer is correct!";
+}
+
+function isCorrect6D() { //Is called when the user selects the correct answer for question 6
+    document.getElementById("Correct6D").innerHTML = "This answer is correct!";
+}
+
+function isCorrect7C() { //Is called when the user selects the correct answer for question 7
+    document.getElementById("Correct7C").innerHTML = "This answer is correct!";
+}
+
+function isCorrect8C() { //Is called when the user selects the correct answer for question 8
+    document.getElementById("Correct8C").innerHTML = "This answer is correct!";
 }
 
 function isIncorrect1A() { //Give feedback if the user selects A for question 1
@@ -15,85 +43,85 @@ function isIncorrect1D() { //Give feedback if the user selects D for question 1
 }
 
 function isIncorrect2A() { //Give feedback if the user selects A for question 2
+
+}
+
+function isIncorrect2B() { //Give feedback if the user selects B for question 2
+
+}
+
+function isIncorrect2C() { //Give feedback if the user selects C for question 2
+
+}
+
+function isIncorrect3A() { //Give feedback if the user selects A for question 3
+
+}
+
+function isIncorrect3B() { //Give feedback if the user selects B for question 3
+
+}
+
+function isIncorrect3D() { //Give feedback if the user selects D for question 3
+
+}
+
+function isIncorrect4A() { //Give feedback if the user selects A for question 4
+
+}
+
+function isIncorrect4C() { //Give feedback if the user selects C for question 4
+
+}
+
+function isIncorrect4D() { //Give feedback if the user selects D for question 4
+
+}
+
+function isIncorrect5A() { //Give feedback if the user selects A for question 5
+
+}
+
+function isIncorrect5C() { //Give feedback if the user selects C for question 5
+
+}
+
+function isIncorrect5D() { //Give feedback if the user selects D for question 5
+
+}
+
+function isIncorrect6A() { //Give feedback if the user selects A for question 6
+
+}
+
+function isIncorrect6B() { //Give feedback if the user selects B for question 6
+
+}
+
+function isIncorrect6C() { //Give feedback if the user selects C for question 6
+
+}
+
+function isIncorrect7A() { //Give feedback if the user selects A for question 7
+
+}
+
+function isIncorrect7B() { //Give feedback if the user selects B for question 7
+
+}
+
+function isIncorrect7D() { //Give feedback if the user selects D for question 7
+
+}
+
+function isIncorrect8A() { //Give feedback if the user selects A for question 8
+
+}
+
+function isIncorrect8B() { //Give feedback if the user selects B for question 8
+
+}
+
+function isIncorrect8D() { //Give feedback if the user selects D for question 8
     
-}
-
-function isIncorrect2B() {
-
-}
-
-function isIncorrect2C() {
-
-}
-
-function isIncorrect3A() {
-
-}
-
-function isIncorrect3B() {
-
-}
-
-function isIncorrect3D() {
-
-}
-
-function isIncorrect4A() {
-
-}
-
-function isIncorrect4C() {
-
-}
-
-function isIncorrect4D() {
-
-}
-
-function isIncorrect5A() {
-
-}
-
-function isIncorrect5C() {
-
-}
-
-function isIncorrect5D() {
-
-}
-
-function isIncorrect6A() {
-
-}
-
-function isIncorrect6B() {
-
-}
-
-function isIncorrect6C() {
-
-}
-
-function isIncorrect7A() {
-
-}
-
-function isIncorrect7B() {
-
-}
-
-function isIncorrect7D() {
-
-}
-
-function isIncorrect8A() {
-
-}
-
-function isIncorrect8B() {
-
-}
-
-function isIncorrect8D() {
-
 }
